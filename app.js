5123cc5c38ba81ef00df1b3c6af5c648f3f77a2d
unknown <delarosauberstrike@gmail.com>
2020-12-26 15:59:58 +0800

    commit these

--- app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const sql = require("mysql");
-const bcrypt = require("bcrypt");
-const urlEncodedParser = bodyParser.urlencoded({extended: false});
-const saltRounds = 10;
-const app = express();
